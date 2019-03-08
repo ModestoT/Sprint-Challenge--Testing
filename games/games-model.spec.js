b16f37335f29c66ff051fc27650ef854360e0f5c
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-03-08 10:46:49 -0600

    Created the testing Database sqlite file and wrote a testing for adding a game to the database

---
+++ games/games-model.spec.js
@@ -0,0 +1,15 @@
+const db = require('../data/dbConfig.js');
+const Games = require('./games-model.js');
+
+describe('Games Model', () => {
+    describe('addGame()', () => {
+        afterEach(async () => {
+            await db('posts').truncate();
+        });
+        it('should add a game to the database and return the game that was added', async () => {
+            const game = await Games.addGame({ title: 'Pacman', genre: 'Arcade', releaseYear: 1980 });
+
+            expect(game).toMatchObject({ title: 'Pacman', genre: 'Arcade', releaseYear: 1980 });
+        }); 
+    })
+});
