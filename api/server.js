05dad8ed56d372a7f231003e364710fa14920289
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-03-08 12:13:36 -0600

    Added a new migration to make the title unique in the table

--- api/server.js
+++ api/server.js
@@ -17,17 +17,17 @@ server.get('/games', async (req, res) => {
 });
 
 server.post('/games', async (req, res) => {
-    if(!req.body.title || !req.body.genre){
-        res.status(422).json({error: 'A Title and Genre are required' });
-    } else {
-        try {
-            const game = await Games.addGame(req.body);
-
-            res.status(200).json(game);
-        } catch(error) {
-            res.status(500).json(error);
+        if(!req.body.title || !req.body.genre){
+            res.status(422).json({error: 'A Title and Genre are required' });
+        } else {
+            try {
+                const game = await Games.addGame(req.body);
+    
+                res.status(200).json(game);
+            } catch(error) {
+                res.status(500).json(error);
+            }
         }
-    }
 });
 
 module.exports = server;
