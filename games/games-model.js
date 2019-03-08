3a913ba08cbe25227420738c3d4b0a2a72a35dd7
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-03-08 10:54:07 -0600

    wrote the function for adding a game to the database based off the test written for it

---
+++ games/games-model.js
@@ -0,0 +1,11 @@
+const db = require('../data/dbConfig.js');
+
+module.exports = {
+    addGame
+};
+
+async function addGame(game){
+    const [id] = await db('games').insert(game, 'id');
+
+    return db('games').where({ id }).first();
+}
--- games/games-model.spec.js
+++ games/games-model.spec.js
@@ -4,7 +4,7 @@ const Games = require('./games-model.js');
 describe('Games Model', () => {
     describe('addGame()', () => {
         afterEach(async () => {
-            await db('posts').truncate();
+            await db('games').truncate();
         });
         it('should add a game to the database and return the game that was added', async () => {
             const game = await Games.addGame({ title: 'Pacman', genre: 'Arcade', releaseYear: 1980 });
