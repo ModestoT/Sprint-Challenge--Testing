14309c8ce77b24383a43fc6a154df5ad9b5ec850
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-03-08 12:46:56 -0600

    droped old games table and made a new table with the title coloumn set to be unique refactored already written test to work with the unique constraint

--- api/server.spec.js
+++ api/server.spec.js
@@ -24,12 +24,12 @@ describe('server.js', () => {
         it('should return a list of games', async () => {
             const expected = [
                 { id: 1, title: 'Pacman', genre: 'Arcade', releaseYear: 1980 },
-                { id: 2, title: 'Pacman', genre: 'Arcade', releaseYear: 1980 },
-                { id: 3, title: 'Pacman', genre: 'Arcade', releaseYear: 1980 }];
+                { id: 2, title: 'Billy', genre: 'Arcade', releaseYear: 1980 },
+                { id: 3, title: 'This', genre: 'Arcade', releaseYear: 1980 }];
             
             await request(server).post('/games').send({ title: 'Pacman', genre: 'Arcade', releaseYear: 1980 });
-            await request(server).post('/games').send({ title: 'Pacman', genre: 'Arcade', releaseYear: 1980 });
-            await request(server).post('/games').send({ title: 'Pacman', genre: 'Arcade', releaseYear: 1980 });
+            await request(server).post('/games').send({ title: 'Billy', genre: 'Arcade', releaseYear: 1980 });
+            await request(server).post('/games').send({ title: 'This', genre: 'Arcade', releaseYear: 1980 });
 
             const res = await request(server).get('/games');
             
--- data/migrations/20190308124100_drop_table.js
+++ data/migrations/20190308124100_drop_table.js
@@ -1,6 +1,6 @@
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('games');
 };
 
 exports.down = function(knex, Promise) {
