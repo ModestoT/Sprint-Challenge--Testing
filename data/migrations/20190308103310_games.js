ef6aad3895d16f956b87aec417a7d38201cade41
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-03-08 10:39:02 -0600

    set up knexfile for the database and set up the games table with the migrations file from knex

---
+++ data/migrations/20190308103310_games.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('games', game => {
+    game.increments();
+
+    game.string('title', 100).notNullable();
+    game.string('genre', 100).notNullable();
+    game.integer('releaseYear');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('games');
+};
